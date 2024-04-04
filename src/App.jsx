0caa2b0937f571870a09373eac05959c8921165f
Bhekisile <bhekisilejozi@gmail.com>
2024-04-04 09:23:56 +0200

    Add routes for the main pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Product from "./pages/Product";
+import Homepage from "./pages/Homepage";
+import Pricing from "./pages/Pricing";
+
 function App() {
   return (
-    <div>
-      Worldwise
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
