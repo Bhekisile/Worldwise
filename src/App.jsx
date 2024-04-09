feb0894025b8ad101a7d1fbe5db3880a79bef2f3
Bhekisile <bhekisilejozi@gmail.com>
2024-04-09 18:01:14 +0200

    Add country list route

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import AppLayout from "./pages/AppLayout";
 import PageNotFound from "./pages/PageNotFound";
 import Login from "./pages/Login";
 import CityList from "./components/CityList";
+import CountryList from "./components/CountryList";
 import { useEffect, useState } from "react";
 
 const BASE_URL = "http://localhost:8000";
@@ -38,8 +39,9 @@ function App() {
         <Route path="pricing" element={<Pricing />} />
         <Route path="login" element={<Login />} />
         <Route path="app" element={<AppLayout />}>
+          <Route index element={<CityList cities={cities} isLoading={isLoading} />} />
           <Route path="cities" element={<CityList cities={cities} isLoading={isLoading} />} />
-          <Route path="countries" element={<CityList cities={cities} isLoading={isLoading} />} />
+          <Route path="countries" element={<CountryList cities={cities} isLoading={isLoading} />} />
           <Route path="form" element={<p>Form</p>} />
         </Route>
         <Route path="*" element={<PageNotFound />} />
