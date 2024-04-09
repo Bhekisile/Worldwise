81b6bce00fc36b95566ce1b8da1329746aed24d8
Bhekisile <bhekisilejozi@gmail.com>
2024-04-09 18:00:40 +0200

    Implementing countries list

---
+++ src/components/CountryList.jsx
@@ -0,0 +1,36 @@
+import CountryItem from "./CountryItem";
+import styles from "./CountryList.module.css";
+import Spinner from "./Spinner";
+import Message from "./Message";
+import PropTypes from "prop-types";
+
+/* eslint-disable */
+function CountryList({ cities, isLoading }) {
+  if (isLoading) return <Spinner />;
+
+  if (!cities.length)
+    return (
+      <Message message="Add your first city byclicking on a city on the map" />
+    );
+
+  const countries = cities.reduce((arr, city) => {
+    if (!arr.map((el) => el.country).includes(city.country))
+      return [...arr, { country: city.country, emoji: city.emoji }];
+    else return arr;
+  }, []);
+
+  return (
+    <ul className={styles.countryList}>
+      {countries.map((country) => (
+        <CountryItem country={country} />
+      ))}
+    </ul>
+  );
+}
+
+CountryList.propTypes = {
+  cities: PropTypes.array.isRequired,
+  isLoading: PropTypes.bool,
+};
+
+export default CountryList;
